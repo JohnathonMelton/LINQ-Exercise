bf05d4314b9b55db9a2afc14457ad63119e13b5b
Johnathon Melton <seventh.acoustic@gmail.com>
2023-10-24 12:39:46 -0500

    LINQ Exercise 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videogameNames = new List<string> {"Elder Scrolls Skyrim", "Ratchet and Clank", "Sly Cooper", "Jak and Daxter", "Final Fantasy", "Dragon Quest"};
+
+            var orderedGames = videogameNames.OrderByDescending(name => name.Length);
+
+            foreach (var videogameName in orderedGames)
+            {
+                Console.WriteLine(videogameName);
+            }
         }
     }
 }
